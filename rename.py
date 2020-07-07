22e4f778a8428ff8c5fa2a205d736461f5d259de
Sam <sksmiller@gmail.com>
2020-07-07 18:46:54 +1200

    Fixed bugs

--- rename.py
+++ rename.py
@@ -15,6 +15,21 @@ class Rename:
         apath = pathlib.Path(path)
         return apath
 
+    def getNameString (self, count, parts, path):
+        if self.theMode == 0:
+            epno = str(count).zfill(2)
+            nameString = f"{parts} - {epno}{path.suffix}"
+        elif self.theMode == 1:
+            nameString = str(count)
+        elif self.theMode == 2:
+            nameString = str(count).zfill(4)
+        elif self.theMode == 3:
+            nameString = re.sub(r"([\s_]?)([\[\(]([^\]\[\(\)]|[\[\(][\]\)])*[\]\)])([\s_]?)",'',path.name)
+        if self.addSpacing:
+            nameString = re.sub(r'([\._])(?=.*\.[^\.]*$)'," ",nameString)
+        
+        return nameString
+
     def preview(self, directory, gui, customName=False):
         print(f'+ {directory}')
         count = self.start
@@ -26,17 +41,7 @@ class Rename:
             # depth = len(path.relative_to(directory).parts)
             # spacer = '    ' * depth
             # print(f'{spacer}+ {path.name}')
-            if self.theMode == 0:
-                epno = str(count).zfill(2)
-                nameString = f"{parts} - {epno}{path.suffix}"
-            elif self.theMode == 1:
-                nameString = str(count)
-            elif self.theMode == 2:
-                nameString = str(count).zfill(4)
-            elif self.theMode == 3:
-                nameString = re.sub(r"(\s?)([\[\(]([^\]\[\(\)]|[\[\(][\]\)])*[\]\)])(\s?)",'',path.name)
-            if self.addSpacing:
-                nameString = re.sub(r'([\._])(?=.*\.[^\.]*$)'," ",nameString)
+            nameString = self.getNameString(count,parts,path)
             gui.insert('', 'end', path.name, text=path.name, values=(nameString,''))
             count += 1
         self.max = count -1
@@ -46,11 +51,12 @@ class Rename:
     def renames(self, directory, name, count, maxcount):
         # count = self.start
         # maxcount = self.max
-        #parts = directory.parts
-        #parts = parts[len(directory.parts)-1]
+        parts = directory.parts
+        parts = parts[len(directory.parts)-1]
         for path in sorted(directory.glob('*.mkv')):
             epno = str(count).zfill(2)
-            nameString = f"{directory}\{name} - {epno}{path.suffix}"
+            nameString = self.getNameString(count,parts,path)
+            nameString = f"{directory}\{nameString}"
             path.rename(nameString)
             if count == maxcount:
                 exit()
